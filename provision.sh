8c0664b7ab4c2fe80ed8e1da1674f2bd5bcf9936
Hannes Koerber <hannes.koerber@gmail.com>
2015-05-16 12:16:05 +0200

    Make privisioning script distribution dependent.

--- provision.sh
+++ provision.sh
@@ -1,27 +1,43 @@
 #!/usr/bin/env bash
 
 _VG='centos'
-_BASE_XML="$HOME/libvirt/base/centos7/centos7.xml"
 _CONNECTION='qemu+ssh://root@10.1.2.4/system'
 
 usage() {
-    echo "$0 <name> [<mac>]" >&2
+    cat << EOF >&2
+$0 <type> <name> [<mac>]
+
+available types:
+debian8
+centos7
+EOF
 }
 
-(( $# < 1 )) && { usage ; exit 1 ; }
+(( $# < 2 )) && { usage ; exit 1 ; }
+type="$1"
+shift
 name="$1"
 shift
 if (( $# >= 1 )) ; then
     mac="$1"
     shift
 else
-    mac="RANDOM"
+    mac="$(printf '52:54:00:%02X:%02X:%02X' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])"
 fi
+
+echo "MAC address: $mac"
+
+case "$type" in
+    "centos7")
+        _BASE_XML="$HOME/libvirt/base/centos7/centos7.xml"
+        ;;
+    "debian8")
+        _BASE_XML="$HOME/libvirt/base/debian8/debian8.xml"
+        ;;
+esac
 
 virt-clone --connect "${_CONNECTION}" \
     --original-xml="${_BASE_XML}" \
     --file "/dev/${_VG}/${name}" \
     --name "${name}" \
     --mac "${mac}" "${@}"
-
-virsh --connect "${_CONNECTION}" dumpxml "${name}" | grep '<mac address'
