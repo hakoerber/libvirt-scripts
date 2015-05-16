1ca23520fee3a60635d0c6f7ca05352e71c3c87e
Hannes Koerber <hannes.koerber@gmail.com>
2015-05-16 23:02:49 +0200

    Add XML schemata for libvirt.

--- provision.sh
+++ provision.sh
@@ -2,6 +2,7 @@
 
 _VG='centos'
 _CONNECTION='qemu+ssh://root@10.1.2.4/system'
+_BASE_XML="$HOME/dev/projects/libvirt-scripts/schema/"
 
 usage() {
     cat << EOF >&2
@@ -25,19 +26,19 @@ else
     mac="$(printf '52:54:00:%02X:%02X:%02X' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])"
 fi
 
-echo "MAC address: $mac"
+echo "MAC: $mac"
 
 case "$type" in
     "centos7")
-        _BASE_XML="$HOME/libvirt/base/centos7/centos7.xml"
+        _xml="${_BASE_XML}/centos7.xml"
         ;;
     "debian8")
-        _BASE_XML="$HOME/libvirt/base/debian8/debian8.xml"
+        _xml="${_BASE_XML}/debian8.xml"
         ;;
 esac
 
 virt-clone --connect "${_CONNECTION}" \
-    --original-xml="${_BASE_XML}" \
+    --original-xml="${_xml}" \
     --file "/dev/${_VG}/${name}" \
     --name "${name}" \
     --mac "${mac}" "${@}"
